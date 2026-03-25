bf53675640ce180717b7a8a1d542e1b63a252897
Teak Slack <teakislack@gmail.com>
2026-03-25 14:08:24 -0400

    remove appbase dependency

--- Dependencies.lua
+++ Dependencies.lua
@@ -1,7 +1,6 @@
 VULKAN_SDK = os.getenv("VULKAN_SDK")
 
 IncludeDir = {}
-IncludeDir["AppBase"] = "%{wks.location}/AppBase/src"
 IncludeDir["VulkanSDK"] = "%{VULKAN_SDK}/Include"
 IncludeDir["spdlog"] = "%{wks.location}/vendor/spdlog/include"
 IncludeDir["glfw"] = "%{wks.location}/vendor/glfw/include"
