869481f01daf13f04a459216dc64a98ed621940e
Teak Slack <teakislack@gmail.com>
2026-04-24 13:54:19 -0400

    [Build] slight cleanup

--- scripts/GenProjects-Win.bat
+++ scripts/GenProjects-Win.bat
@@ -1,4 +1,4 @@
 @echo off
 cd ..\
-cmake -S . -B build -G "Visual Studio 18 2026" -A x64
+cmake -S . -B build -G "Visual Studio 18 2026" -A x64 -DCOMPILE_WITH_VULKAN=ON -DCOMPILE_WITH_DX12=ON
 pause
